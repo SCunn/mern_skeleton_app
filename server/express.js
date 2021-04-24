c2e3e22fb98149f712c8e0edcfb99e821e1e0296
scunn <shanecunn@hotmail.com>
2021-04-24 15:27:00 +0100

    Starting the server

--- server/express.js
+++ server/express.js
@@ -3,7 +3,7 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
-import helmet from 'helmut'
+import helmet from 'helmet'
 
 const app = express()
     
