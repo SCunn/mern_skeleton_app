3d88ba3832badd456a95a30d4bdb7057bb8d157d
scunn <shanecunn@hotmail.com>
2021-04-28 12:28:55 +0100

    Authorizing signed in users, Auth error handling for express-jwt

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -58,6 +58,15 @@ const requireSignin = expressJwt({
     userProperty: 'auth'
 })
 
-const hasAuthorization = (req, res) => {}
+const hasAuthorization = (req, res, next) => {
+    const authorized = req.profile && req.auth && req.profile._id == req.auth._id
+
+    if (!(authorized)) {
+        return res.status('403').json({
+            error: "User is not authorized"
+        })
+    }
+    next()
+}
 
 export default { signin, signout, requireSignin, hasAuthorization }
