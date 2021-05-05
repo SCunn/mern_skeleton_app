c3a1426da743d13749bee76483f76c10df514c40
scunn <shanecunn@hotmail.com>
2021-05-06 00:42:48 +0100

    Signup component

--- client/user/Signup.js
+++ client/user/Signup.js
@@ -14,6 +14,7 @@ import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import {Link} from 'react-router-dom'
+import { values } from 'lodash'
 
 
 const useStyles = makeStyles(theme => ({
@@ -51,4 +52,88 @@ export default function Signup() {
         open: false,
         error: ''
     })
+
+
+  // This handleChange function takes the new value that is entered in the input field and sets it as the state
+  const handleChange = name => event => {
+      setValues({ ...values, [name]: event.target.value })
+  }
+
+  // The clickSubmit function is  called when the form is submitted
+  const clickSubmit = () => {
+    const user = {
+      name: values.name || undefined,
+      email: values.email || undefined,
+      password: values.password || undefined
+    }
+    create(user).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error })
+      } else {
+        setValues({ ...values, error: '', open: true })
+      }
+    })
+  }
+
+  return(
+    <div>
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography variant="h6" className={classes.title}>
+            Sign Up
+          </Typography>
+          <TextField id="name" label="Name" className={classes.textField}
+          value={values.name}
+          onChange={handleChange('name')}
+          margin="normal"
+          />
+          <br/>
+          <TextField id="email" type="email" label="Email" 
+          className={classes.textField} 
+          value={values.email} 
+          onChange={handleChange('email')}
+          margin="normal"
+          />
+          <br/>
+          <TextField id="password" type="password" label="Password"
+          className={classes.textField}
+          value={values.password}
+          onChange={handleChange('password')}
+          margin="normal"
+          />
+          <br/>
+          {
+            values.error && (<Typography component="p" color="error">
+              <Icon color="error" className={classes.error}>error</Icon>
+                {values.error}
+            </Typography>)
+          }
+        </CardContent>
+        <CardActions>
+          <Button 
+            color="primary" 
+            variant="contained" 
+            onClick={clickSubmit} 
+            className={classes.submit}
+            >Submit</Button>
+        </CardActions>
+      </Card>
+      <Dialog open={values.open} disableBackdropClick={true}>
+        <DialogTitle>New Account</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            New account successfully created.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Link to="/signin">
+            <Button color="primary" autoFocus="autoFocus" variant="contained">
+              Sign In
+            </Button>
+          </Link>
+        </DialogActions>
+      </Dialog>
+    </div>
+  )
+
 } 
