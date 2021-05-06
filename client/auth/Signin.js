4a5a84fa66f030fd5a5821636414915fb7a34ac3
SCUNN <shanecunn@hotmail.com>
2021-05-06 10:57:19 +0100

    Signin.js & signin route added

---
+++ client/auth/Signin.js
@@ -0,0 +1,8 @@
+export default function Signin(props) {
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+        error: '',
+        redirectToReferrer:false
+    })
+}
